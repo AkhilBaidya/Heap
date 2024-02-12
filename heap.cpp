9d5c1a9d076200b7094d87406b6f2666c8223782
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2024-02-12 10:32:12 -0800

    How to go down heap (mainly made going down to child1 take precedence, but in the case where child2 is the only one larger than the value we want to add, go down child2)

--- heap.cpp
+++ heap.cpp
@@ -36,8 +36,40 @@ void add(node* val, node* array[100]) {
 
   else {
 
-    
+    while (current != NULL) {
 
+      if (current -> child1 == NULL) {
+	child1 = val; //if the next child 1 is empty, just add the input there
+      }
+
+      else if (current -> child2 == NULL) {
+	child2 = val; //otherwise, if the next child 2 is empty, just add the input there
+      }
+
+      else { //else move down the binary tree
+
+	int myval = val -> value;
+	int lefval = current -> child1 -> value;
+	int rightval = current -> child2 -> value;
+	
+	if (leftval > myval || leftval == myval  && rightval !> myval || leftval < myval && rightval < myval) { //going down the left takes precedence
+
+	  current = current -> child1;
+	}
+
+	else if (leftval == myval && rightval > myval || leftval < myval && rightval !< myval) { //otherwise go right
+
+	  current = current -> child2;
+	}
+
+	else {
+
+	  cout << "Error when going down binary tree" << endl;
+	  
+	}
+	
+      }
+    }
   }
 }
 
